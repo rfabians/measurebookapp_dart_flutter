5c56813d2a177585b728ec5315f4d48e2d02276a
Rafael Fabian Sanchez Osorio <rfsanchezo@gmail.com>
2020-02-02 21:47:07 -0500

    Se implementa SinglechildScrollView para corregir error de renderizado al girar la Pantalla

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class _MyAppState extends State<MyApp> {
       setState(() {
         _isLoggedIn = true;
         idUser = _googleSignIn.currentUser.id;
+        //print(idUser);
       });
     } catch (err){
       print(err);
@@ -42,159 +43,115 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         body: Center(
+          child: SingleChildScrollView(
             child: _isLoggedIn
-                ? 
-                Column(
-                    
+                ? Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    // Implementación de Interfaz cuando el usuario ya está logeado
                     children: <Widget>[
-                      Divider(height: 75,),
-                      Image.network(_googleSignIn.currentUser.photoUrl, height: 100.0, width: 100.0,),
-                      Divider(),
-                      Text(_googleSignIn.currentUser.displayName, style: TextStyle(
-                        fontFamily: 'Roboto',
-                        color: Color(0xff007FFF),
-                      ),
-                      ),
+                      Image.network(_googleSignIn.currentUser.photoUrl, height: 100.0,),
+                      SizedBox(height: 10.0,),
                       Text(_googleSignIn.currentUser.email, style: TextStyle(
                         fontFamily: 'Roboto',
                         color: Color(0xff898989),
-                      ),
-                      ),
-                      Divider(height: 10.0,),
-                      ConstrainedBox(
-                        constraints: new BoxConstraints(
-                          minHeight: 350,
-                          maxHeight: 351,
-                        ),
-                        child: new ListView(
-                          shrinkWrap: true,
-                          children: <Widget>[
-                          ],
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: <Widget>[
-                        Column(
-                            children: <Widget>[
-                            Image.asset('assets/nuevo.png', height: 50,),
-                            Divider(height: 5.0,),
-                            Text('Nuevo Proyecto', style: TextStyle(
-                              fontFamily: 'Roboto',
-                              color: Color(0xff007FFF),
-                            ),)
-                          ],
-                        ),
-                        Column(
-                          children: <Widget>[
-                            Image.asset('assets/delta.png', height: 50.0,),
-                            Text('Navegar red \npasiva Igac', style: TextStyle(
-                              fontFamily: 'Roboto',
-                              color: Color(0xff007FFF),
-                            ),)
-                          ],
-                        )
-                      ],
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Image.asset('assets/borrar.png', height: 50.0,),
-                            Text('Borrar Proyecto', style: TextStyle(
-                              fontFamily: 'Roboto',
-                              color: Color(0xff007FFF),
-                            ),)
-                        ],
-                      ),
-                      Divider(height: 10),
-                      FlatButton( child: Text('Cerrar Sesión',style: TextStyle(
+                        fontSize: 14.0,
+                      ),),
+                      SizedBox(height: 10.0,),
+                      Text(_googleSignIn.currentUser.displayName, style: TextStyle(
                         fontFamily: 'Roboto',
-                        color: Colors.white,
-                      )
+                        color: Color(0xff007FFF),
+                        fontSize: 15.0,
+                      ),),
+                      SizedBox(height: 20.0,),
+                      Divider(),
+                      Container(
+                        height: 400,
+                        child: ListView(
+
+                        ),
                       ),
-                      color: Color(0xff007FFF),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(15.0)
-                      ),
-                      onPressed: (){
+                      Divider(),
+                      FlatButton(
+                        shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+                        child: Text('Cerrar Sesión', style: TextStyle(
+                          fontFamily: 'Roboto',
+                          fontSize: 13.0,
+                          color: Colors.white,
+                        ),),
+                        color: Color(0xff007FFF),
+                        onPressed: (){
                         _logout();
-                      },)
-                    ],
-                  )
-                : Container(
-                  child: ConstrainedBox(
-                  constraints: BoxConstraints.expand(),
-                  child: FlatButton(
-                  onPressed: () {
-                    _login();
-                  },
-                  padding: EdgeInsets.all(0.0),
-                  child: 
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                      Image.asset('assets/logo.png', height: 120.0,),
-                      RichText(
-                        text: TextSpan(
-                          style: DefaultTextStyle.of(context).style,
-                          children: <TextSpan> [
-                            TextSpan(
-                              text: 'Tu cartera de ', style: TextStyle(
-                                fontFamily: 'Roboto',
-                                color: Color(0xff898989),
-                              )
-                            ),
-                            TextSpan(text: 'Topografía ', style:  TextStyle(
-                              fontFamily: 'Roboto',
-                              color: Color(0xff007FFF),
-                            )),
-                            TextSpan(text: 'ahora en tu dispositivo ', style: TextStyle(
-                                fontFamily: 'Roboto',
-                                color: Color(0xff898989),
-                              )
-                              ),
-                            TextSpan(text: 'Movil', style:  TextStyle(
-                              fontFamily: 'Roboto',
-                              color: Color(0xff007FFF),
-                            )),
-                          ],
-                        )
-                      ),
-                      Image.asset('assets/google.png', height: 50.0),
-                      RichText(
-                        text: TextSpan(
-                          style: DefaultTextStyle.of(context).style,
-                          children: <TextSpan> [
-                            TextSpan(
-                              text: 'Ingresa a la ', style: TextStyle(
-                                fontFamily: 'Roboto',
-                                color: Color(0xff898989),
-                              )
-                            ),
-                            TextSpan(text: 'APP ', style:  TextStyle(
-                              fontFamily: 'Roboto',
-                              color: Color(0xff007FFF),
-                            )),
-                            TextSpan(text: 'con tu cuenta de ', style: TextStyle(
-                                fontFamily: 'Roboto',
-                                color: Color(0xff898989),
-                              )
-                              ),
-                            TextSpan(text: 'Google', style:  TextStyle(
-                              fontFamily: 'Roboto',
-                              color: Color(0xff007FFF),
-                            )),
-                          ],
-                        )
-                      ),
+                      })
                     ],
                   ),
-                  
-                  )
-                  )  
-                  )
-                ), 
+                )
+              :Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  // Implementación de Interfaz para Login
+                  children: <Widget>[
+                    Image.asset('assets/logo.png', height: 100,),
+                    SizedBox(height: 40.0,),
+                    RichText(text: TextSpan(
+                      children: <TextSpan> [
+                        TextSpan(text: 'Tu Cartera de ', style: TextStyle(
+                          fontFamily: 'Roboto',
+                          height: 12.0,
+                          color: Color(0xff898989),
+                        )),
+                        TextSpan(text: 'Topografía', style: TextStyle(
+                          fontFamily: 'Roboto',
+                          color: Color(0xff007FFF),
+                          height: 12.0,
+                        )),
+                        TextSpan(text: ', ahora en tu dispositivo ', style: TextStyle(
+                          fontFamily: 'Roboto',
+                          color: Color(0xff898989),
+                          height: 12.0,
+                        )),
+                        TextSpan(text: 'Movil', style: TextStyle(
+                          fontFamily: 'Roboto',
+                          color: Color(0xff007FFF),
+                          height: 12.0,
+                        ))
+                      ]
+                    )),
+                    SizedBox(height: 40.0,),
+                    FlatButton(onPressed: (){
+                      _login();
+                    }, child: Image.asset('assets/google.png', height: 50.0,)),
+                    SizedBox(height: 40.0,),
+                    RichText(text: TextSpan(
+                      children: <TextSpan> [
+                        TextSpan(text: 'Ingresa a la ', style: TextStyle(
+                          fontFamily: 'Roboto',
+                          color: Color(0xff898989),
+                          height: 12.0,
+                        )),
+                        TextSpan(text: 'APP ', style: TextStyle(
+                          fontFamily: 'Roboto',
+                          color: Color(0xff007FFF),
+                          height: 12.0,
+                        )),
+                        TextSpan(text: 'con tu cuenta de ', style: TextStyle(
+                          fontFamily: 'Roboto',
+                          color: Color(0xff898989),
+                          height: 12.0,
+                        )),
+                        TextSpan(text: 'Google', style: TextStyle(
+                          fontFamily: 'Roboto',
+                          color: Color(0xff007FFF),
+                          height: 12.0,
+                        ))
+                      ]
+                    )),
+                  ],
                 ),
-      );
+              )
+          )
+        )
+      )
+    );
   }
 }
