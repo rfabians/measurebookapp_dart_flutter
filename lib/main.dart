bdc66ee991acbe5836bd81e3e2599780a6b9c4f0
Rafael Fabian Sanchez Osorio <rfsanchezo@gmail.com>
2020-02-20 20:58:28 -0500

    Pagina Nuevo Proyecto

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'pages/proyecto_nuevo.dart';
-
+}
 void main() => runApp(MeasureBookAPP());
 
 //
