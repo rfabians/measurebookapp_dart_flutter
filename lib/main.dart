20e4e7c8bd515fc7ab97cec435afb65af063fc9c
Rafael Fabian Sanchez Osorio <rfsanchezo@gmail.com>
2020-03-08 00:02:08 -0500

    Cambio Color APP

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'pages/proyecto_nuevo.dart';
-}
+
 void main() => runApp(MeasureBookAPP());
 
 //
@@ -55,6 +55,7 @@ class _MyAppState extends State<IniciarSesion> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Color(0xff292929),
         body: Center(
           child: SingleChildScrollView(
             child: _isLoggedIn
@@ -63,11 +64,12 @@ class _MyAppState extends State<IniciarSesion> {
                     mainAxisAlignment: MainAxisAlignment.start,
                     // Implementación de Interfaz cuando el usuario ya está logeado
                     children: <Widget>[
+                      SizedBox (height: 50.0),
                       Image.network(_googleSignIn.currentUser.photoUrl, height: 100.0,),
                       SizedBox(height: 10.0,),
                       Text(_googleSignIn.currentUser.email, style: TextStyle(
                         fontFamily: 'Roboto',
-                        color: Color(0xff898989),
+                        color: Color(0xffBEBEBE),
                         fontSize: 14.0,
                       ),),
                       SizedBox(height: 10.0,),
@@ -101,7 +103,7 @@ class _MyAppState extends State<IniciarSesion> {
                         child: Text('Nueva Pantalla', style: TextStyle(
                           fontFamily: 'Roboto',
                           fontSize: 13.0,
-                          color: Colors.white,
+                          color: Color(0xffBEBEBE),
                         ),),
                         color: Color(0xff007FFF),
                         onPressed: (){
@@ -118,14 +120,14 @@ class _MyAppState extends State<IniciarSesion> {
                   mainAxisAlignment: MainAxisAlignment.start,
                   // Implementación de Interfaz para Login
                   children: <Widget>[
-                    Image.asset('assets/logo.png', height: 100,),
+                    Image.asset('assets/images/logo.png', height: 100,),
                     SizedBox(height: 40.0,),
                     RichText(text: TextSpan(
                       children: <TextSpan> [
                         TextSpan(text: 'Tu Cartera de ', style: TextStyle(
                           fontFamily: 'Roboto',
                           height: 12.0,
-                          color: Color(0xff898989),
+                          color: Color(0xffBEBEBE),
                         )),
                         TextSpan(text: 'Topografía', style: TextStyle(
                           fontFamily: 'Roboto',
@@ -134,7 +136,7 @@ class _MyAppState extends State<IniciarSesion> {
                         )),
                         TextSpan(text: ', ahora en tu dispositivo ', style: TextStyle(
                           fontFamily: 'Roboto',
-                          color: Color(0xff898989),
+                          color: Color(0xffBEBEBE),
                           height: 12.0,
                         )),
                         TextSpan(text: 'Movil', style: TextStyle(
@@ -147,13 +149,13 @@ class _MyAppState extends State<IniciarSesion> {
                     SizedBox(height: 40.0,),
                     FlatButton(onPressed: (){
                       _login();
-                    }, child: Image.asset('assets/google.png', height: 50.0,)),
+                    }, child: Image.asset('.../images/google.png', height: 50.0,)),
                     SizedBox(height: 40.0,),
                     RichText(text: TextSpan(
                       children: <TextSpan> [
                         TextSpan(text: 'Ingresa a la ', style: TextStyle(
                           fontFamily: 'Roboto',
-                          color: Color(0xff898989),
+                          color: Color(0xffBEBEBE),
                           height: 12.0,
                         )),
                         TextSpan(text: 'APP ', style: TextStyle(
@@ -163,7 +165,7 @@ class _MyAppState extends State<IniciarSesion> {
                         )),
                         TextSpan(text: 'con tu cuenta de ', style: TextStyle(
                           fontFamily: 'Roboto',
-                          color: Color(0xff898989),
+                          color: Color(0xffBEBEBE),
                           height: 12.0,
                         )),
                         TextSpan(text: 'Google', style: TextStyle(
